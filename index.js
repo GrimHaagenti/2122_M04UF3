823cc3df23514d82b3c0e54dad9d9e28b302e6cb
enti <enti@enti.enti>
2022-02-04 14:35:31 +0100

    Clase 04/02, terminar juego, importar imagenes necesarias, buscar manera mejor

---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/node
+ 
+let http = require("http");
+
+let fs = require("fs");
+
+
+let http_server = http.createServer(function(req, res) {
+ 	
+
+	if (req.url == "/character.png")
+	{
+
+	fs.readFile("character.png" , function(err,data)
+		{
+		res.writeHead(200);		
+		res.end(data);
+		})
+	}
+
+	else{  
+
+
+	fs.readFile("index.html", function(err, data)
+		{
+		if (err)
+			{
+			console.log("ERROR!!");
+			return;
+			}
+
+	 	res.writeHead(200);
+	
+		res.end(data);
+
+	})
+
+	
+	
+	}}).listen(3000);
+		 
